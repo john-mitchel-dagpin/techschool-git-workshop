164b395be96ead252a7afab026d13c3ca4f078b9
John Mitchel Dagpin <johnmd@stud.ntnu.no>
2025-09-16 19:17:43 +0200

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,9 @@
+/**
+ * Function that greets a person
+ * @param firstname First name
+ * @param lastname Last name
+ */
+
+export const greeting = (firstname: string, lastname: string) => {
+    console.log(`Hello ${firstname} ${lastname}`);
+};
