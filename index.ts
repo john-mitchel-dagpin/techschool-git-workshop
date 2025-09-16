911ee2fef0d44abc2401a81ad4a155b7e451812f
John Mitchel Dagpin <johnmd@stud.ntnu.no>
2025-09-16 19:43:43 +0200

    Delete index.ts

--- index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Function that greets a person
- * @param firstname First name
- * @param lastname Last name
- */
-
-export const greeting = (firstname: string, lastname: string) => {
-    console.log(`Hello ${firstname} ${lastname}`);
-};
